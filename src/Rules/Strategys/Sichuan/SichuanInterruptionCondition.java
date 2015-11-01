b009da143d0ccbdbc0d7ccaea36e03e017f84877
rémi  <remirobert33530@gmail.com>
2015-11-01 10:16:50 +0800

    fix bug algo 😝🎃💥

--- src/Rules/Strategys/Sichuan/SichuanInterruptionCondition.java
+++ src/Rules/Strategys/Sichuan/SichuanInterruptionCondition.java
@@ -39,7 +39,6 @@ public class SichuanInterruptionCondition implements IInterruptionCondition {
                     return true;
                 }
             }
-            numberSuit += 1;
         }
         return false;
     }
